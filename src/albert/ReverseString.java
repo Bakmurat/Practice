718be59dab1e6d404389cacd91e68c8f004f1840
Bakmurat <bakmuratkubanaliev@gmail.com>
2022-07-24 21:47:50 -0400

    reverse string complete

--- src/albert/ReverseString.java
+++ src/albert/ReverseString.java
@@ -2,10 +2,18 @@ package albert;
 
 public class ReverseString {
 
+    public static String reverseString(String str){
+        String reverse="";
+        for (int i=str.length()-1; i>=0; i--){
+            reverse+=""+str.charAt(i);
+        }
+
+        return reverse;
+    }
+
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hi");
-        System.out.println("second");
-        System.out.println("third");
+
+        System.out.println("reverseString(\"Hello World\") = " + reverseString("Hello World"));
     }
 }
