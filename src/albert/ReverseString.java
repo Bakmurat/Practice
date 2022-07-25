df7cfb4c646e3987cd33a9d90ea75fc94b332299
Bakmurat <bakmuratkubanaliev@gmail.com>
2022-07-24 21:36:10 -0400

    second commit

---
+++ src/albert/ReverseString.java
@@ -0,0 +1,10 @@
+package albert;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hi");
+        System.out.println("second");
+    }
+}
